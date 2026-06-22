c2e56d9222ad1f47c6ef06e09ac75f02477bd122
Jevith Reddy <jevithgns@gmail.com>
2026-06-22 21:07:00 +0530

    add conversation system

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -1,8 +1,15 @@
 import { serve } from "inngest/next";
-import { inngest } from "../../../inngest/client";
-import { demoGenerate } from "@/inngest/functions";
 
+import { inngest } from "@/inngest/client";
+import { demoError, demoGenerate } from "@/inngest/functions";
+import { processMessage } from "@/features/conversations/inngest/process-message";
+
+// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [demoGenerate],
+  functions: [
+    demoGenerate,
+    demoError,
+    processMessage,
+  ],
 });
